f18d378f287e646aed27d4310d17e2f85ac71efb
Rafael Henrique Menegon <12224578@FAJ2.ACAD.LOCAL>
2023-08-17 21:28:05 -0300

    feat: initial commit

---
+++ src/main/java/br/edu/faj/poo/ministerio/MinisterioService/MinisterioServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.faj.poo.ministerio.MinisterioService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinisterioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinisterioServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/faj/poo/ministerio/MinisterioService/MinisterioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.faj.poo.ministerio.MinisterioService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinisterioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
